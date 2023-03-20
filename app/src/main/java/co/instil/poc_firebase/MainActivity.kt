66430615ba0fb91b1ddaa7498a78fcdb36dab587
Annie Rash <annie.morony@willowtreeapps.com>
2023-03-20 11:45:21 -0400

    fixes missing SDK info, adds google services json

--- app/src/main/java/co/instil/poc_firebase/MainActivity.kt
+++ app/src/main/java/co/instil/poc_firebase/MainActivity.kt
@@ -1,5 +1,6 @@
 package co.instil.poc_firebase
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,6 +20,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import co.instil.poc_firebase.ui.theme.POC_FirebaseTheme
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,84 +38,97 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
 
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomNavigationBar(navController) },
-        content = { padding ->
-            Box(modifier = Modifier.padding(padding)) {
-                Navigation(navController = navController)
-            }
-        },
-        backgroundColor = colorResource(id = R.color.purple_200)
-    )
-
-}
-
-@Composable
-fun Navigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = NavigationItem.Home.route) {
-        composable(NavigationItem.Home.route) {
-            HomeScreen()
-        }
-        composable(NavigationItem.Account.route) {
-            AccountScreen()
-        }
-        composable(NavigationItem.Shop.route) {
-            ShopScreen()
-        }
-        composable(NavigationItem.Billing.route) {
-            BillingScreen()
-        }
-        composable(NavigationItem.Help.route) {
-            HelpScreen()
-        }
-    }
-}
-
-@Composable
-fun BottomNavigationBar(navController: NavController) {
-    val items = listOf(
-        NavigationItem.Home,
-        NavigationItem.Account,
-        NavigationItem.Shop,
-        NavigationItem.Billing,
-        NavigationItem.Help
-    )
-    BottomNavigation(
-        backgroundColor = colorResource(id = R.color.white),
-        contentColor = colorResource(id = R.color.purple_700)
-    ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        items.forEach { item ->
-            BottomNavigationItem(
-                icon = {
-                    Icon(
-                        painterResource(id = item.icon),
-                        contentDescription = item.title
-                    )
-                },
-                label = { Text(text = item.title) },
-                selectedContentColor = colorResource(id = R.color.teal_200),
-                unselectedContentColor = colorResource(id = R.color.teal_700),
-                selected = currentRoute == item.route,
-                onClick = {
-                    navController.navigate(item.route) {
-                        navController.graph.startDestinationRoute?.let { route ->
-                            popUpTo(route) {
-                                saveState = true
-                            }
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
+        Firebase.dynamicLinks
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData: PendingDynamicLinkData? ->
+                //deeplink from result (which may be null if no link found)
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
                 }
-            )
+                // Handle the deep link. For example, open the linked
+                // content, or apply promotional credit to the user's
+                // account.
+            }
+    }
+
+    @Composable
+    fun MainScreen(modifier: Modifier = Modifier) {
+        val navController = rememberNavController()
+        Scaffold(
+            bottomBar = { BottomNavigationBar(navController) },
+            content = { padding ->
+                Box(modifier = Modifier.padding(padding)) {
+                    Navigation(navController = navController)
+                }
+            },
+            backgroundColor = colorResource(id = R.color.purple_200)
+        )
+
+    }
+
+    @Composable
+    fun Navigation(navController: NavHostController) {
+        NavHost(navController = navController, startDestination = NavigationItem.Home.route) {
+            composable(NavigationItem.Home.route) {
+                HomeScreen()
+            }
+            composable(NavigationItem.Account.route) {
+                AccountScreen()
+            }
+            composable(NavigationItem.Shop.route) {
+                ShopScreen()
+            }
+            composable(NavigationItem.Billing.route) {
+                BillingScreen()
+            }
+            composable(NavigationItem.Help.route) {
+                HelpScreen()
+            }
+        }
+    }
+
+    @Composable
+    fun BottomNavigationBar(navController: NavController) {
+        val items = listOf(
+            NavigationItem.Home,
+            NavigationItem.Account,
+            NavigationItem.Shop,
+            NavigationItem.Billing,
+            NavigationItem.Help
+        )
+        BottomNavigation(
+            backgroundColor = colorResource(id = R.color.white),
+            contentColor = colorResource(id = R.color.purple_700)
+        ) {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+            items.forEach { item ->
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            painterResource(id = item.icon),
+                            contentDescription = item.title
+                        )
+                    },
+                    label = { Text(text = item.title) },
+                    selectedContentColor = colorResource(id = R.color.teal_200),
+                    unselectedContentColor = colorResource(id = R.color.teal_700),
+                    selected = currentRoute == item.route,
+                    onClick = {
+                        navController.navigate(item.route) {
+                            navController.graph.startDestinationRoute?.let { route ->
+                                popUpTo(route) {
+                                    saveState = true
+                                }
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
+            }
         }
     }
 }
